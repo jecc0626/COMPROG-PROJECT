2a9945411ec1248ba44f53ffbe83a4f80abf6f45
fryon123 <86453292+RhonieshJhun@users.noreply.github.com>
2021-07-05 13:02:47 +0800

    Fixed.

--- calculator.py
+++ calculator.py
@@ -1,1 +1,9 @@
-# Calculator Logic
+class Calculator:
+    def __init__(self):
+        pass
+
+    def Evaluate(self, inp):
+        try:
+            return eval(inp)
+        except:
+            return "Error"
